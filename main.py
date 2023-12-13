d6e3dd311aeab214c793e8b2dc67d6975a7477a1
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-14 01:12:17 +0200

    Task

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
-print("Hello, enter the number of meters!")
-number = int(input("How many meters: "))
+# print("Hello, enter 3 numbers!")
+# number1 = int(input("Enter 1 number: "))
+# number2 = int(input("Enter 2 number: "))
+# number3 = int(input("Enter 3 number: "))
+#
+# choice = input("Choose an action (max, min, avg): ")
+#
+# if choice == "max":
+#     result = max(number1, number2, number3)
+#     print("Maximum: ", result)
+# elif choice == "min":
+#     result = min(number1, number2, number3)
+#     print("Minimal: ", result)
+# elif choice == "avg":
+#     result = (number1 + number2 + number3) / 3
+#     print("Arithmetic average: ", result)
+# else:
+#     print("Wrong question!")
 
-choice = input("What to translate (mile, inch, yard): ")
-
-if choice == "mile":
-     result = (number) * 0.00062137
-     print("Mile: ", result)
-elif choice == "inch":
-     result = (number) * 39.37
-     print("Inch: ", result)
-elif choice == "yard":
-     result = (number) * 1.0936
-     print("Yard: ", result)
-else:
-     print("Wrong question!")
+# print("Hello, enter the number of meters!")
+# number = int(input("How many meters: "))
+#
+# choice = input("What to translate (mile, inch, yard): ")
+#
+# if choice == "mile":
+#      result = (number) * 0.00062137
+#      print("Mile: ", result)
+# elif choice == "inch":
+#      result = (number) * 39.37
+#      print("Inch: ", result)
+# elif choice == "yard":
+#      result = (number) * 1.0936
+#      print("Yard: ", result)
+# else:
+#      print("Wrong question!")
 
