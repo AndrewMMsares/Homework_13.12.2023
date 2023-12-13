e6565893baf3d3ae11c6a64836c749534872599d
Andrei Kalinichenko <muse2140@gmail.com>
2023-12-14 01:08:38 +0200

    Task 1

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-print("Hello, enter 3 numbers!")
-number1 = int(input("Enter 1 number: "))
-number2 = int(input("Enter 2 number: "))
-number3 = int(input("Enter 3 number: "))
+print("Hello, enter the number of meters!")
+number = int(input("How many meters: "))
 
-choice = input("Choose an action (max, min, avg): ")
+choice = input("What to translate (mile, inch, yard): ")
 
-if choice == "max":
-    result = max(number1, number2, number3)
-    print("Maximum: ", result)
-elif choice == "min":
-    result = min(number1, number2, number3)
-    print("Minimal: ", result)
-elif choice == "avg":
-    result = (number1 + number2 + number3) / 3
-    print("Arithmetic average: ", result)
+if choice == "mile":
+     result = (number) * 0.00062137
+     print("Mile: ", result)
+elif choice == "inch":
+     result = (number) * 39.37
+     print("Inch: ", result)
+elif choice == "yard":
+     result = (number) * 1.0936
+     print("Yard: ", result)
 else:
-    print("Wrong question!")
+     print("Wrong question!")
 
